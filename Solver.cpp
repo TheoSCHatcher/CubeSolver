56f43e6ee07e1ea1b906965bf410099ef565144c
TheoSCHatcher <tsch4@cam.ac.uk>
2025-04-04 20:30:14 +0100

    Added print cube net function

--- Solver.cpp
+++ Solver.cpp
@@ -111,24 +111,26 @@ void printCubeNet(CubeState& state){
     auto stateEdges = state.edgePermutation;
     auto sEO = state.edgeOrientation;
 
-    std::cout << "               " << cornerColours[stateCorners[2]][sCO[2]] << "  " << edgeColours[stateEdges[3]][sEO[3]] << "  " << cornerColours[stateCorners[1]][sCO[1]] << "\n";
-    std::cout << "               " << edgeColours[stateEdges[2]][sEO[2]] << "  W  " << edgeColours[stateEdges[0]][sEO[0]] << "\n";
-    std::cout << "               " << cornerColours[stateCorners[3]][sCO[3]] << "  " << edgeColours[stateEdges[1]][sEO[1]] << "  " << cornerColours[stateCorners[0]][sCO[0]] << "\n";
-    std::cout << "              " << "--------------\n"; 
+    std::cout << "          " << cornerColours[stateCorners[2]][sCO[2]] << "  " << edgeColours[stateEdges[3]][sEO[3]] << "  " << cornerColours[stateCorners[1]][sCO[1]] << "\n";
+    std::cout << "          " << edgeColours[stateEdges[2]][sEO[2]] << "  W  " << edgeColours[stateEdges[0]][sEO[0]] << "\n";
+    std::cout << "          " << cornerColours[stateCorners[3]][sCO[3]] << "  " << edgeColours[stateEdges[1]][sEO[1]] << "  " << cornerColours[stateCorners[0]][sCO[0]] << "\n";
+    std::cout << "         " << "---------\n"; 
 
-    std::cout <<  corners[stateCorners[2]] << "  " << edges[stateEdges[2]] << "  " << cornerColours[stateCorners[3]][mod1(sCO[3]+1,3)]  << " | " << cornerColours[stateCorners[3]][mod1(sCO[3]+2,3)] << "  " << edgeColours[stateEdges[1]][mod1(1+sEO[1],2)] << "  " << cornerColours[stateCorners[0]][mod1(sCO[0]+1,3)] << " | " \
-                <<  corners[stateCorners[0]] << "  " << edges[stateEdges[0]] << "  " << corners[stateCorners[1]]  << " | " << corners[stateCorners[1]] << "  " << edges[stateEdges[3]] << "  " << corners[stateCorners[2]]  << "\n";
+    std::cout <<  cornerColours[stateCorners[2]][mod1(sCO[2]+2,3)] << "  " << edgeColours[stateEdges[2]][mod1(sEO[2]+1,2)] << "  " << cornerColours[stateCorners[3]][mod1(sCO[3]+1,3)]  << " | " << cornerColours[stateCorners[3]][mod1(sCO[3]+2,3)] << "  " << edgeColours[stateEdges[1]][mod1(1+sEO[1],2)] << "  " << cornerColours[stateCorners[0]][mod1(sCO[0]+1,3)] << " | " \
+                <<  cornerColours[stateCorners[0]][mod1(2+sCO[0],3)] << "  " << edgeColours[stateEdges[0]][mod1(sEO[0]+1,2)] << "  " << cornerColours[stateCorners[1]][mod1(sCO[1]+1,3)]  << " | " << cornerColours[stateCorners[1]][mod1(sCO[1]+2,3)] << "  " << edgeColours[stateEdges[3]][mod1(sEO[0]+1,2)] << "  " << cornerColours[stateCorners[2]][mod1(sCO[2]+1,3)]  << "\n";
 
-    std::cout <<  edges[stateEdges[11]] << "   L   " << edges[stateEdges[9]]                                    << "  | " << edgeColours[stateEdges[9]][sEO[9]] << "  G  " << edgeColours[stateEdges[8]][sEO[8]] << " | "\
-                << edges[stateEdges[8]] << "   R   " << edges[stateEdges[10]]                                    << "  | " << edges[stateEdges[10]] << "   B   " << edges[stateEdges[11]] << "\n";
+    std::cout <<  edgeColours[stateEdges[11]][mod1(sEO[11]+1,2)] << "  O  " << edgeColours[stateEdges[9]][mod1(sEO[9]+1,2)] << " | " \
+                << edgeColours[stateEdges[9]][sEO[9]] << "  G  " << edgeColours[stateEdges[8]][sEO[8]] << " | "\
+                << edgeColours[stateEdges[8]][mod1(1+sEO[8],2)] << "  R  " << edgeColours[stateEdges[10]][mod1(1+sEO[10],2)] << " | " \
+                << edgeColours[stateEdges[10]][sEO[10]] << "  B  " << edgeColours[stateEdges[11]][sEO[11]] << "\n";
 
-    std::cout <<  corners[stateCorners[6]] << "  " << edges[stateEdges[6]] << "  " << corners[stateCorners[7]]  << " | " << cornerColours[stateCorners[7]][mod1(1+sCO[7],3)] << "  " << edgeColours[stateEdges[5]][mod1(1+sEO[5],2)] << "  " << cornerColours[stateCorners[4]][mod1(sCO[0]+2,3)] << " | " \
-                <<  corners[stateCorners[4]] << "  " << edges[stateEdges[4]] << "  " << corners[stateCorners[5]]  << " | " << corners[stateCorners[5]] << "  " << edges[stateEdges[7]] << "  " << corners[stateCorners[6]] << "\n";
+    std::cout <<  cornerColours[stateCorners[6]][mod1(sCO[6]+1,3)] << "  " << edgeColours[stateEdges[6]][mod1(sCO[6]+1,2)] << "  " << cornerColours[stateCorners[7]][mod1(sCO[7]+2,3)]  << " | " << cornerColours[stateCorners[7]][mod1(1+sCO[7],3)] << "  " << edgeColours[stateEdges[5]][mod1(1+sEO[5],2)] << "  " << cornerColours[stateCorners[4]][mod1(sCO[0]+2,3)] << " | " \
+                <<  cornerColours[stateCorners[4]][mod1(sCO[4]+1, 3)] << "  " << edgeColours[stateEdges[4]][mod1(sEO[4]+1,2)] << "  " << cornerColours[stateCorners[5]][mod1(sCO[5]+2,3)]  << " | " << cornerColours[stateCorners[5]][mod1(sCO[5]+1,3)] << "  " << edgeColours[stateEdges[7]][mod1(sEO[7]+1,2)] << "  " << cornerColours[stateCorners[6]][mod1(sCO[6]+2,3)] << "\n";
     
-    std::cout << "              " << "--------------\n"; 
-    std::cout << "               " << corners[stateCorners[7]] << "  " << edges[stateEdges[5]] << "  " << corners[stateCorners[4]] << "\n";
-    std::cout << "               " << edges[stateEdges[6]] << "   D   " << edges[stateEdges[4]] << "\n";
-    std::cout << "               " << corners[stateCorners[6]] << "  " << edges[stateEdges[7]] << "  " << corners[stateCorners[5]] << "\n";
+    std::cout << "         " << "---------\n"; 
+    std::cout << "          " << cornerColours[stateCorners[7]][sCO[7]] << "  " << edgeColours[stateEdges[5]][sEO[5]] << "  " << cornerColours[stateCorners[4]][sCO[4]] << "\n";
+    std::cout << "          " << edgeColours[stateEdges[6]][sEO[6]] << "  Y  " << edgeColours[stateEdges[4]][sEO[4]] << "\n";
+    std::cout << "          " << cornerColours[stateCorners[6]][sCO[6]] << "  " << edgeColours[stateEdges[7]][sEO[7]] << "  " << cornerColours[stateCorners[5]][sCO[5]] << "\n"; 
 }
 
 
@@ -239,6 +241,7 @@ int main() {
 
     //applyMoves(SolvedCube, 14);
     applyMoves(SolvedCube, 1);
+    applyMoves(SolvedCube, 0);
     applyMoves(SolvedCube, 2);
 
 
